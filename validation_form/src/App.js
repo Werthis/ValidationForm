29c0cb3394521918bef35f62b20b9f5e86d2a677
Werthis <lukaszstawecki@gmail.com>
2021-12-22 22:31:53 +0100

    vip

--- validation_form/src/App.js
+++ validation_form/src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+// import InputAdornment from "@material-ui/core/InputAdornment";
+import { Grid, Container, Menu, MenuItem, Button } from "@material-ui/core";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+  textField: {
+    width: "25ch",
+  },
+}));
+
+const App = () => {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    type: "",
+    identyNumber: "",
+    photo: "",
+  });
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [type, setType] = useState("");
+  const [idNumber, setIdNumber] = useState("Numer");
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleMenuItemClick = (event, newType) => {
+    setType(newType);
+    newType === "person" ? setIdNumber('Numer PESEL') : setIdNumber('Numer NIP');
+    setAnchorEl(null);
+  };
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  
+
+  const classes = useStyles();
+
+  console.log(type);
+  console.log(idNumber);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Container maxWidth="sm">
+        <Grid
+          container
+          spacing={2}
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
         >
-          Learn React
-        </a>
-      </header>
+          <Grid container item xs={12}>
+            <form noValidate autoComplete="off">
+              <TextField id="outlined-basic" label="Imię" variant="outlined" />
+            </form>
+          </Grid>
+          <Grid container item xs={12}>
+            <form noValidate autoComplete="off">
+              <TextField
+                id="outlined-basic"
+                label="Nazwisko"
+                variant="outlined"
+              />
+            </form>
+          </Grid>
+          <Grid container item xs={12}>
+            <Button
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+              onClick={handleClick}
+            >
+              Typ
+            </Button>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              value={type}
+            >
+              <MenuItem
+                onClick={(event) => handleMenuItemClick(event, "person")}
+              >
+                Osoba Prywatna
+              </MenuItem>
+              <MenuItem
+                onClick={(event) => handleMenuItemClick(event, "company")}
+              >
+                Firma
+              </MenuItem>
+            </Menu>
+          </Grid>
+          <Grid container item xs={12}>
+            <form noValidate autoComplete="off">
+              <TextField
+                id="outlined-basic"
+                label={idNumber}
+                variant="outlined"
+              />
+            </form>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
