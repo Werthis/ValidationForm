43a261e76f02e8dc8ee5d16b18bdce34a3b355ba
Werthis <lukaszstawecki@gmail.com>
2021-12-25 23:25:43 +0100

    vip

--- validation_form/src/App.js
+++ validation_form/src/App.js
@@ -11,8 +11,19 @@ import { Grid, Container, Menu, MenuItem, Button } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
+    // display: "flex",
     flexWrap: "wrap",
+    textAlign: "center",
+    flexGrow: 1,
+    justifyContent: "space-evenly",
+    background: "#FFFFF0",
+    // justifyContent: "center",
+    alignItems: "center",
+  },
+  gridContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    background: "#FFFFF0",
   },
   margin: {
     margin: theme.spacing(1),
@@ -40,12 +51,17 @@ const App = () => {
   const [lastName, setLastName] = useState("");
   const [type, setType] = useState("");
   const [typeName, setTypeName] = useState("TYP");
-  const [idNumberLabel, setIdNumberLabel] = useState();
+  const [idNumberLabel, setIdNumberLabel] = useState(
+    <div style={{ height: 80 }} />
+  );
   const [dropzoneInfo, setDropzoneInfo] = useState(
     "Drag and drop an image here or click"
   );
   const [nip, setNip] = useState("");
   const [pesel, setPesel] = useState("");
+  const [errorFirstNameSubmit, setErrorFirstNameSubmit] = useState(false);
+  const [errorLastNameSubmit, setErrorLastNameSubmit] = useState(false);
+  const [errorTypeSubmit, setErrorTypeSubmit] = useState(false);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -59,47 +75,79 @@ const App = () => {
     setFirstName(firstName);
   };
 
-  const error = nip.length === 10 ? false : true;
+  // const error = nip.length === 10 ? false : true;
+
+  const handleSubmit = (event) => {
+    // if (firstName === "") {
+    //   setErrorFirstNameSubmit(true);
+    // }
+    // if (lastName === "") {
+    //   setErrorLastNameSubmit(true);
+    // }
+    if (
+      (type === "person" && pesel === "") ||
+      (type === "company" && nip === "")
+    ) {
+      setErrorTypeSubmit(true);
+    } else {
+      // setErrorFirstNameSubmit(false);
+      // setErrorLastNameSubmit(false);
+      setErrorTypeSubmit(false);
+      setValues({
+        firstName: firstName,
+        lastName: lastName,
+        type: type,
+        identyNumber: type === "person" ? pesel : nip,
+      });
+    }
+  };
 
   const handleMenuItemClick = (event, newType) => {
     setType(newType);
     if (newType === "person") {
       setIdNumberLabel(
-        <Grid container item xs={12}>
+        <Grid
+          container
+          item
+          xs={12}
+          style={{ height: 80 }}
+          className={classes.gridContainer}
+        >
           <TextField
             className={classes.textField}
             id="idNumber"
             label="Numer PESEL"
             variant="outlined"
             onChange={(event) => setPesel(event.target.value)}
+            helperText={errorTypeSubmit ? "Niepoprawny numer Pesel" : ""}
+            error={errorTypeSubmit}
           />
         </Grid>
       );
       setTypeName("Osoba Prywatna");
     } else {
       setIdNumberLabel(
-        <TextField
-          className={classes.textField}
-          id="idNumber"
-          label="Numer NIP"
-          variant="outlined"
-          onChange={(event) => setNip(event.target.value)}
-          helperText={error ? "Niepoprawny numer NIP" : ""}
-          error={error}
-        />
+        <Grid
+          container
+          item
+          xs={12}
+          style={{ height: 80 }}
+          className={classes.gridContainer}
+        >
+          <TextField
+            className={classes.textField}
+            id="idNumber"
+            label="Numer NIP"
+            variant="outlined"
+            onChange={(event) => setNip(event.target.value)}
+            helperText={errorTypeSubmit ? "Niepoprawny numer NIP" : ""}
+            error={errorTypeSubmit}
+          />{" "}
+        </Grid>
       );
       setTypeName("Firma");
     }
     setAnchorEl(null);
-  };
-
-  const handleSubmit = (event) => {
-    setValues({
-      firstName: firstName,
-      lastName: lastName,
-      type: type,
-      identyNumber: type === "person" ? pesel : nip,
-    });
   };
 
   const classes = useStyles();
@@ -107,77 +155,77 @@ const App = () => {
   // console.log("type", type);
   // console.log("firstName", firstName);
   // console.log("lastName", lastName);
-  console.log("NIP", nip);
+  // console.log("NIP", nip);
   // console.log("PESEL", pesel);
   console.log("values", values);
+  console.log(errorFirstNameSubmit);
+  console.log(errorLastNameSubmit);
+  console.log(errorTypeSubmit);
+
+  // console.log(setErrorFirstNameSubmit);
 
   return (
-    <div className={classes.root}>
-      <Container maxWidth="sm">
-        <Grid
-          container
-          spacing={2}
-          direction="column"
-          // justifyContent="center"
-          // alignItems="center"
-        >
-          <Grid item xs={12}>
-            <form noValidate autoComplete="off">
-              <TextField
-                className={classes.textField}
-                id="firstName"
-                label="Imię"
-                variant="outlined"
-                onChange={(event) => setFirstName(event.target.value)}
-              />
-            </form>
-          </Grid>
-          <Grid item xs={12}>
-            <form noValidate autoComplete="off">
-              <TextField
-                className={classes.textField}
-                id="lastName"
-                label="Nazwisko"
-                placeholder="wprowadź nazwisko"
-                variant="outlined"
-                onChange={(event) => setLastName(event.target.value)}
-              />
-            </form>
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              aria-controls="simple-menu"
-              aria-haspopup="true"
-              onClick={handleClick}
+    <Container className={classes.root}>
+      <Grid container spacing={2} direction="column">
+        <Grid item xs={12}>
+          <form noValidate autoComplete="off">
+            <TextField
+              className={classes.textField}
+              id="firstName"
+              label="Imię"
+              variant="outlined"
+              onChange={(event) => setFirstName(event.target.value)}
+              helperText={errorFirstNameSubmit ? "Wpisz imię" : ""}
+              error={errorFirstNameSubmit}
+            />
+          </form>
+        </Grid>
+        <Grid item xs={12}>
+          <form noValidate autoComplete="off">
+            <TextField
+              className={classes.textField}
+              id="lastName"
+              label="Nazwisko"
+              // placeholder="wprowadź nazwisko"
+              variant="outlined"
+              onChange={(event) => setLastName(event.target.value)}
+              helperText={errorLastNameSubmit ? "Wpisz nazwisko" : ""}
+              error={errorLastNameSubmit}
+            />
+          </form>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            aria-controls="menu"
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            {typeName}
+          </Button>
+          <Menu
+            id="menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            value={type}
+          >
+            <MenuItem onClick={(event) => handleMenuItemClick(event, "person")}>
+              OSOBA PRYWATNA
+            </MenuItem>
+            <MenuItem
+              onClick={(event) => handleMenuItemClick(event, "company")}
             >
-              {typeName}
-            </Button>
-            <Menu
-              id="simple-menu"
-              anchorEl={anchorEl}
-              keepMounted
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-              value={type}
-            >
-              <MenuItem
-                onClick={(event) => handleMenuItemClick(event, "person")}
-              >
-                OSOBA PRYWATNA
-              </MenuItem>
-              <MenuItem
-                onClick={(event) => handleMenuItemClick(event, "company")}
-              >
-                FIRMA
-              </MenuItem>
-            </Menu>
-          </Grid>
-          <Grid item xs={12}>
-            <form noValidate autoComplete="off">
-              {idNumberLabel}
-            </form>
-          </Grid>
-          {/* <Grid
+              FIRMA
+            </MenuItem>
+          </Menu>
+        </Grid>
+        <Grid item xs={12}>
+          <form noValidate autoComplete="off">
+            {idNumberLabel}
+          </form>
+        </Grid>
+        {/* <Grid
             container
             alignContent="center"
             justifyContent="center"
@@ -210,34 +258,22 @@ const App = () => {
               onChange={(files) => console.log("Files:", files)}
             />
           </Grid>{" "} */}
-          {/* <Grid container item xs={12}>
+        {/* <Grid container item xs={12}>
             {/* <Dropzone></Dropzone> */}
-          {/* </Grid> */}
-          <Grid item xs={12}>
-            <Button
-              variant="contained"
-              color="primary"
-              // href="https://localhost:60001/Contractor/Save"
-              onClick={(event) => {
-                setValues({
-                  firstName: firstName,
-                  lastName: lastName,
-                  type: type,
-                  identyNumber: type === "person" ? pesel : nip,
-                });
-              }}
-            >
-              Link
-            </Button>{" "}
-          </Grid>
+        {/* </Grid> */}
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="inherit"
+            border-radius="20px"
+            // href="https://localhost:60001/Contractor/Save"
+            onClick={handleSubmit}
+          >
+            Link
+          </Button>{" "}
         </Grid>
-      </Container>{" "}
-      {/* <Router>
-        /* <Switch> */}
-      {/* <Route component={PageNotFound} /> */}
-      {/* </Switch> */}
-      {/* </Router> */}
-    </div>
+      </Grid>
+    </Container>
   );
 };
 
